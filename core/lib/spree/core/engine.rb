b26ad68aab8a1d62ebb4626e3d714f4d420a6f4f
Rafael Izidoro <izidoro.rafa@gmail.com>
2015-05-15 11:09:42 -0300

    add 2-4-stable promotions

--- core/lib/spree/core/engine.rb
+++ core/lib/spree/core/engine.rb
@@ -71,7 +71,10 @@ module Spree
           Spree::Promotion::Rules::Product,
           Spree::Promotion::Rules::User,
           Spree::Promotion::Rules::FirstOrder,
-          Spree::Promotion::Rules::UserLoggedIn]
+          Spree::Promotion::Rules::UserLoggedIn,
+          Spree::Promotion::Rules::OneUsePerUser,
+          Spree::Promotion::Rules::Taxon
+        ]
       end
 
       initializer 'spree.promo.register.promotions.actions' do |app|
